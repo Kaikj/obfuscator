38c96bd7e57132a84873e402a321529c813d1e64
peining <peining_pan@hotmail.com>
2016-11-07 15:10:22 +0800

    Add in random adds/subtracts before binary operators that do not affect the actual binary operator instruction

--- lib/Transforms/Obfuscation/DeadCodeInsertion.cpp
+++ lib/Transforms/Obfuscation/DeadCodeInsertion.cpp
@@ -3,26 +3,29 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Obfuscation/Utils.h"
 #include "llvm/IR/Intrinsics.h"
+#include <iostream>
 
 #define DEBUG_TYPE "dci"
-#define NUMBER_DCI 2
+#define NUMBER_DCI 1
 
 namespace {
 	struct DeadCodeInsertion : public FunctionPass {
 		static char ID; // Pass identification, replacement for typeid	
 		void (DeadCodeInsertion::*funcDeadCodeInsert[NUMBER_DCI])(BinaryOperator *bo);
+		Instruction::BinaryOps opcodeList[2];		
 		bool flag;		
 		DeadCodeInsertion() : FunctionPass(ID) {}
 		DeadCodeInsertion(bool flag) : FunctionPass(ID) {
-
 			this->flag = flag;
 			funcDeadCodeInsert[0] = &DeadCodeInsertion::addZero;
-			funcDeadCodeInsert[1] = &DeadCodeInsertion::subZero;	
+//			funcDeadCodeInsert[1] = &DeadCodeInsertion::addIf;
+			opcodeList[0] = Instruction::Add;
+			opcodeList[1] = Instruction::Sub;
 		}
 		bool runOnFunction(Function &F);
 		void insertDeadCode(Function *F);
 		void addZero(BinaryOperator *bo);
-		void subZero(BinaryOperator *bo);
+		void addIf(BinaryOperator *bo);
 	};
 }
 
@@ -30,7 +33,7 @@ char DeadCodeInsertion::ID = 0;
 static RegisterPass<DeadCodeInsertion> X("dci", "Dead Code Insertion Pass");
 
 Pass *llvm::createDeadCodeInsertion(bool flag) {
-  return new DeadCodeInsertion(flag);
+	return new DeadCodeInsertion(flag);
 }
 
 bool DeadCodeInsertion::runOnFunction(Function &F) {
@@ -46,22 +49,16 @@ void DeadCodeInsertion::insertDeadCode(Function *f) {
 	for (Function::iterator bb = tmp->begin(); bb != tmp->end(); ++bb) {
 	      for (BasicBlock::iterator inst = bb->begin(); inst != bb->end(); ++inst) {
 			if (inst->isBinaryOp()) {
-				  switch (inst->getOpcode()) {
-				  case BinaryOperator::Add:
-				    // case BinaryOperator::FAdd:
-				    // Substitute with random add operation
-				    (this->*funcDeadCodeInsert[llvm::cryptoutils->get_range
-					(NUMBER_DCI)])(cast<BinaryOperator>(inst));
-				    break;
-				  case BinaryOperator::Sub:
-				    // case BinaryOperator::FSub:
-				    // Substitute with random sub operation
-				    (this->*funcDeadCodeInsert[llvm::cryptoutils->get_range
-					(NUMBER_DCI)])(cast<BinaryOperator>(inst));
-				    break;
-				  default:
-				    break;
-				  }              // End switch
+				switch (inst->getOpcode()) {
+				case BinaryOperator::Add:
+				case BinaryOperator::Sub:
+					// case BinaryOperator::FAdd:
+					// Substitute with random add operation
+					(this->*funcDeadCodeInsert[llvm::cryptoutils->get_range(NUMBER_DCI)])(cast<BinaryOperator>(inst));
+					break;
+				default:
+					break;
+			  	}              // End switch
 			}                // End isBinaryOp
 		}                  // End for basickblock
 	}                    // End for Function
@@ -70,25 +67,31 @@ void DeadCodeInsertion::insertDeadCode(Function *f) {
 void DeadCodeInsertion::addZero(BinaryOperator *bo){
 	BinaryOperator *op = NULL;
 	Type *ty = bo->getType();
-	ConstantInt *c1 = (ConstantInt *)ConstantInt::get(ty, 0);
-	op = BinaryOperator::Create(Instruction::Add, bo->getOperand(0), c1, "", bo);
-	op = BinaryOperator::Create(bo->getOpcode(), op, bo->getOperand(1), "", bo);
+	Instruction::BinaryOps opcode = opcodeList[llvm::cryptoutils->get_range(2)];
+	ConstantInt *c1 = (ConstantInt *)ConstantInt::get(ty, llvm::cryptoutils->get_uint64_t());
+	int index = llvm::cryptoutils->get_range(2);
+	op = BinaryOperator::Create(opcode, bo->getOperand(index), c1, "", bo);
+
+	//to continue
+	bool toContinue = llvm::cryptoutils->get_range(2);
+	while (toContinue) {
+		opcode = opcodeList[llvm::cryptoutils->get_range(2)];
+		Value *operands[3];	
+		operands[0] = bo->getOperand(0);
+		operands[1] = bo->getOperand(1);
+		operands[2] = op;
+		Value *LHS_operand = operands[llvm::cryptoutils->get_range(3)];
+		Value *RHS_operand = operands[llvm::cryptoutils->get_range(3)];
+		op = BinaryOperator::Create(opcode, LHS_operand, RHS_operand, "", bo);
+		toContinue = llvm::cryptoutils->get_range(2);
+	
+	}
 	// Check signed wrap
 	op->setHasNoSignedWrap(bo->hasNoSignedWrap());
 	op->setHasNoUnsignedWrap(bo->hasNoUnsignedWrap());
-	bo->replaceAllUsesWith(op);
+	//bo->replaceAllUsesWith(op);
 }
 
-void DeadCodeInsertion::subZero(BinaryOperator *bo){
-	BinaryOperator *op = NULL;
-	Type *ty = bo->getType();
-	ConstantInt *c1 = (ConstantInt *)ConstantInt::get(ty, 0);
-	op = BinaryOperator::Create(Instruction::Sub, bo->getOperand(0), c1, "", bo);
-	op = BinaryOperator::Create(bo->getOpcode(), op, bo->getOperand(1), "", bo);
-	// Check signed wrap
-	op->setHasNoSignedWrap(bo->hasNoSignedWrap());
-	op->setHasNoUnsignedWrap(bo->hasNoUnsignedWrap());
-	bo->replaceAllUsesWith(op);
+void DeadCodeInsertion::addIf(BinaryOperator *bo){
 }
-
 
