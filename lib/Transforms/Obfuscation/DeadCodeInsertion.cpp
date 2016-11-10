1f76540c3db88507cd811b03660665c68ef5bd9f
peining <peining_pan@hotmail.com>
2016-11-10 22:44:46 +0800

    Adapt insert used dead code to work with floating operations too

--- lib/Transforms/Obfuscation/DeadCodeInsertion.cpp
+++ lib/Transforms/Obfuscation/DeadCodeInsertion.cpp
@@ -44,8 +44,13 @@ namespace {
 		void insertUnusedDeadCode(Function *F);
 		void addUnusedInstruction(BinaryOperator *bo);
 		BasicBlock* createAlteredBasicBlock(BasicBlock*, const Twine&, Function*);
-		Constant* getConstant(Type *ty);
+		Constant* getRandomConstant(Type *ty);
+		Constant* getConstant(Type *ty, int number);
 		Instruction::BinaryOps getRandomOpcode(Type *ty);
+		Instruction::BinaryOps getRandomAddOrSubOpcode(Type *ty);
+		bool isAdditionOpcode(Instruction::BinaryOps op);
+		Instruction::BinaryOps getAdditionOpcode(Type *ty);
+		Instruction::BinaryOps getSubtractionOpcode(Type *ty);
 	};
 }
 
@@ -84,24 +89,25 @@ void DeadCodeInsertion::insertUsedDeadCodeIntoBlock(BasicBlock * bb) {
 		//do not always randomise for each instruction
 		//1/5 chance of not inserting in dead code
 		int prob_insert_dead_code = llvm::cryptoutils->get_range(5);
+		//add scam modifications
 		if (!isa<TerminatorInst>(inst) && modifiedDefinitions.size() > 0 && prob_insert_dead_code) {
 			unsigned int random = llvm::cryptoutils->get_range(modifiedDefinitions.size());
 			ModifiedDefinition *md = modifiedDefinitions.at(random);
-			Type *int_type = Type::getInt32Ty(bb->getContext());
-			//quantity to add
-			unsigned int random_increment = llvm::cryptoutils->get_range(100);
-			ConstantInt *c1 = (ConstantInt *)ConstantInt::get(int_type, random_increment);
-			Instruction::BinaryOps opcode = opcodeList[llvm::cryptoutils->get_range(2)];
+			Type* ty = md->modifiedValue->getType();
+			int random_increment = llvm::cryptoutils->get_range(100);
+			Constant* c1 = getConstant(ty, random_increment);
+			Instruction::BinaryOps opcode = getRandomAddOrSubOpcode(ty);
 			BinaryOperator *new_inst =  BinaryOperator::Create(opcode, md->modifiedValue, c1, "scam", inst);				
 			ModifiedDefinition *mdNew = new ModifiedDefinition();
 			mdNew->modifiedValue = new_inst;
-			if (opcode == Instruction::Add) {
+			if (isAdditionOpcode(opcode)) {
 				mdNew->increment = md->increment + random_increment;
 			} else {
 				mdNew->increment = md->increment - random_increment;
 			}
 			modifiedDefinitions.at(random) = mdNew;
 		}
+		//correct modifications made
 		for (unsigned int i = 0; i < inst->getNumOperands(); i++) {
 			Value *v = inst->getOperand(i);
 			//if present in original Defn
@@ -112,22 +118,22 @@ void DeadCodeInsertion::insertUsedDeadCodeIntoBlock(BasicBlock * bb) {
 				//add instruction that negates modified definition before the inst
 				Value* latestValue = modifiedDef->modifiedValue;
 				int increment = modifiedDef->increment;
+				Type* ty = latestValue->getType();
 				Instruction::BinaryOps opcode;
 				if (increment == 0) {
 					continue;				
 				} else if (increment < 0) {
 					//do addition to get it back
-					opcode = BinaryOperator::Add;		
+					opcode = getAdditionOpcode(ty);		
 				} else {
 					//do subtraction to get it back 
-					opcode = BinaryOperator::Sub;				
+					opcode = getSubtractionOpcode(ty);				
 				}
 				unsigned int final_increment = abs(increment);
-				Type *int_type = Type::getInt32Ty(bb->getContext());
 				//break correction into 2 steps
 				unsigned int step = llvm::cryptoutils->get_range(final_increment);
-				ConstantInt *c1 = (ConstantInt *)ConstantInt::get(int_type, step);
-				ConstantInt *c2 = (ConstantInt *)ConstantInt::get(int_type, final_increment - step);
+				Constant *c1 = getConstant(ty, step);
+				Constant *c2 = getConstant(ty, final_increment - step);
 				BinaryOperator *new_inst =  BinaryOperator::Create(opcode, latestValue, c1, "correct-", inst);
 				BinaryOperator *new_inst_2 =  BinaryOperator::Create(opcode, new_inst, c2, "correct2-", inst);
 				//replace uses
@@ -145,7 +151,7 @@ void DeadCodeInsertion::insertUsedDeadCodeIntoBlock(BasicBlock * bb) {
 		}
 		//if it's a load or a binary operator
 		//store Value into original definitions and modified Definitions so that we can modify next time
-		if ((isa<LoadInst>(inst) || isa<BinaryOperator>(inst)) && inst->getType()->isIntegerTy()) {
+		if ((isa<LoadInst>(inst) || isa<BinaryOperator>(inst)) && (inst->getType()->isIntegerTy() || inst->getType()->isFloatingPointTy())) {
 			originalDefinitions.push_back(inst);
 			ModifiedDefinition* md = new ModifiedDefinition();
 			md->modifiedValue = inst;
@@ -189,7 +195,7 @@ void DeadCodeInsertion::addUnusedInstruction(BinaryOperator *bo){
 	BinaryOperator *op = NULL;
 	Type *ty = bo->getType();
 	Instruction::BinaryOps opcode = getRandomOpcode(ty);
-	Constant *c1 = getConstant(ty);
+	Constant *c1 = getRandomConstant(ty);
 	int index = llvm::cryptoutils->get_range(2);
 	op = BinaryOperator::Create(opcode, bo->getOperand(index), c1, "unusedOne", bo);
 
@@ -221,11 +227,48 @@ Instruction::BinaryOps DeadCodeInsertion::getRandomOpcode(Type *ty) {
 	}
 }
 
-Constant* DeadCodeInsertion::getConstant(Type *ty){
+Instruction::BinaryOps DeadCodeInsertion::getRandomAddOrSubOpcode(Type *ty) {
+	if (ty->isFloatingPointTy()) {
+		return fopcodeList[llvm::cryptoutils->get_range(2)];
+	} else {
+		return opcodeList[llvm::cryptoutils->get_range(2)];	
+	}
+}
+
+Instruction::BinaryOps DeadCodeInsertion::getAdditionOpcode(Type *ty) {
+	if (ty->isFloatingPointTy()) {
+		return Instruction::FAdd;
+	} else {
+		return Instruction::Add;	
+	}
+}
+
+Instruction::BinaryOps DeadCodeInsertion::getSubtractionOpcode(Type *ty) {
+	if (ty->isFloatingPointTy()) {
+		return Instruction::FSub;
+	} else {
+		return Instruction::Sub;	
+	}
+}
+
+bool DeadCodeInsertion::isAdditionOpcode(Instruction::BinaryOps op) {
+	return op == Instruction::Add || op == Instruction::FAdd;
+}
+
+Constant* DeadCodeInsertion::getRandomConstant(Type *ty){
 	if (ty->isFloatingPointTy()) {
 		return (ConstantFP *)ConstantFP::get(ty, (double) llvm::cryptoutils->get_uint64_t());
 	} else {
 		return (ConstantInt*) ConstantInt::get(ty, llvm::cryptoutils->get_uint64_t());
 	}
 }
+
+Constant* DeadCodeInsertion::getConstant(Type *ty, int number){
+	if (ty->isFloatingPointTy()) {
+		return (ConstantFP *)ConstantFP::get(ty, (double) number);
+	} else {
+		return (ConstantInt*) ConstantInt::get(ty, number);
+	}
+}
+
 
