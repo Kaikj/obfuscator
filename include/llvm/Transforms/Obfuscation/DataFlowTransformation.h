2ce66451e5d99de6c5d9991f2614d3c4cd053a39
kaikj <bluesword246@gmail.com>
2016-11-12 02:33:28 +0800

    Default set of includes used, inspired from existing files

--- include/llvm/Transforms/Obfuscation/DataFlowTransformation.h
+++ include/llvm/Transforms/Obfuscation/DataFlowTransformation.h
@@ -14,4 +14,24 @@
 #ifndef _DATAFLOWTRANSFORMATION_INCLUDES_
 #define _DATAFLOWTRANSFORMATION_INCLUDES_
 
+// LLVM include
+#include "llvm/Pass.h"
+#include "llvm/IR/Function.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/Transforms/Utils/Local.h" // For DemoteRegToStack and DemotePHIToStack
+#include "llvm/Transforms/IPO.h"
+#include "llvm/Transforms/Scalar.h"
+#include "llvm/IR/Module.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/CryptoUtils.h"
+#include "llvm/Transforms/Obfuscation/Utils.h"
+
+// Namespace
+using namespace std;
+
+namespace llvm {
+  Pass *createDataFlowTransformation();
+  Pass *createDataFlowTransformation(bool flag);
+}
+
 #endif
