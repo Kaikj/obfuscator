1db6c6f8034885f4c0a71952d210d5ec812650f1
kaikj <bluesword246@gmail.com>
2016-11-12 02:31:54 +0800

    Add DataFlowTransformation files

---
+++ include/llvm/Transforms/Obfuscation/DataFlowTransformation.h
@@ -0,0 +1,17 @@
+//===- DataFlowTransformation.h - Data Flow Transformation Obfuscation pass------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===---------------------------------------------------------------------------------------===//
+//
+// This file contains includes and defines for the data flow transformation pass
+//
+//===---------------------------------------------------------------------------------------===//
+
+#ifndef _DATAFLOWTRANSFORMATION_INCLUDES_
+#define _DATAFLOWTRANSFORMATION_INCLUDES_
+
+#endif
---
+++ lib/Transforms/Obfuscation/DataFlowTransformation.cpp
@@ -0,0 +1,14 @@
+//===- DataFlowTransformation.cpp - Data Flow Transformation Obfuscation pass----===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------------===//
+//
+// This file implements the data flow transformation pass
+//
+//===----------------------------------------------------------------------------===//
+
+#include "llvm/Transforms/Obfuscation/DataFlowTransformation.h"
