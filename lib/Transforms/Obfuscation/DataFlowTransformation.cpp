a303fc36798cf5ab13177206ff43908c74489b4d
kaikj <bluesword246@gmail.com>
2016-11-14 17:10:32 +0800

    Implement array indirection

--- lib/Transforms/Obfuscation/DataFlowTransformation.cpp
+++ lib/Transforms/Obfuscation/DataFlowTransformation.cpp
@@ -12,6 +12,8 @@
 //===----------------------------------------------------------------------------===//
 
 #include "llvm/Transforms/Obfuscation/DataFlowTransformation.h"
+#include "llvm/ADT/APSInt.h"
+#include "llvm/ADT/APInt.h"
 
 #define DEBUG_TYPE "dataflowtransformation"
 
@@ -19,55 +21,119 @@ using namespace llvm;
 
 STATISTIC(Transformed, "Data flow transformed");
 
+// static cl::opt<string> FunctionName();
+
 namespace {
-struct DataFlowTransformation : public FunctionPass {
+struct DataFlowTransformation : public ModulePass {
   static char ID;
   bool flag;
 
-  DataFlowTransformation() : FunctionPass(ID) {}
-  DataFlowTransformation(bool flag) : FunctionPass(ID) { this->flag = flag; }
+  // Declaration of global array name
+  Twine globalArrayBaseName = "dataFlowTransArray";
 
-  bool doInitialization(Module &M);
-  bool runOnFunction(Function &F);
+  DataFlowTransformation() : ModulePass(ID) {}
+
+  bool runOnModule(Module &M);
+  void runOnFunction(Function &F);
+
+  Twine getArrayName(Module &M);
 };
 }
 
 char DataFlowTransformation::ID = 0;
 static RegisterPass<DataFlowTransformation> X("dataflowtransformation", "Data flow transformation");
+APSInt currentIndex = APSInt(APInt(32, 0));
+std::map<APSInt, ConstantInt *> intToIndex;
 
 // Public exposed function
 Pass *llvm::createDataFlowTransformation() {
   return new DataFlowTransformation();
 }
 
-Pass *llvm::createDataFlowTransformation(bool flag) {
-  return new DataFlowTransformation(flag);
-}
-
-// Declaration of global array name
-Twine globalArrayName = "dataFlowTransArray";
-
-bool DataFlowTransformation::doInitialization(Module &M) {
+bool DataFlowTransformation::runOnModule(Module &M) {
   // Type definitions
-  ArrayType *IntArrayType = ArrayType::get(IntegerType::get(M.getContext(), 32), 10);
+  ArrayType *IntArrayType_0 = ArrayType::get(IntegerType::get(M.getContext(), 32), 0);
 
   // Global variable definitions
-  GlobalVariable *globalArray = new GlobalVariable(
-    M,
-    IntArrayType,
-    false,
-    GlobalValue::ExternalLinkage,
-    0,
-    globalArrayName + "_" + M.getModuleIdentifier()
-  );
+  StringRef arrayName = getArrayName(M).str();
+  M.getOrInsertGlobal(arrayName, IntArrayType_0);
+  GlobalVariable *globalArray = M.getNamedGlobal(arrayName);
+  globalArray->setConstant(false);
+  globalArray->setLinkage(GlobalValue::ExternalLinkage);
 
   // Set initial values
   ConstantInt *zero = ConstantInt::get(M.getContext(), APInt(32, 0));
   globalArray->setInitializer(zero);
+
+  for (Module::iterator F = M.begin(), FE = M.end(); F != FE; ++F) {
+    runOnFunction(*F);
+  }
+
+  std::vector<Constant *> v(intToIndex.size(), 0);
+  for (std::map<APSInt, ConstantInt *>::iterator it = intToIndex.begin(), ite = intToIndex.end(); it != ite; ++it) {
+    outs() << v[*(it->second->getValue().getRawData())] << "\n";
+    v[*(it->second->getValue().getRawData())] = ConstantInt::get(M.getContext(), it->first);
+  }
+  // Type definitions
+  ArrayType *IntArrayType_size = ArrayType::get(IntegerType::get(M.getContext(), 32), intToIndex.size());
+
+  // Global variable definitions
+  StringRef newArrayName = (getArrayName(M) + "new").str();
+  M.getOrInsertGlobal(newArrayName, IntArrayType_size);
+  GlobalVariable *newGlobalArray = M.getNamedGlobal(newArrayName);
+  newGlobalArray->setConstant(false);
+  newGlobalArray->setLinkage(GlobalValue::ExternalLinkage);
+
+  // Set initial values
+  Constant *values = ConstantArray::get(IntArrayType_size, ArrayRef<Constant *>(v));
+  newGlobalArray->setInitializer(values);
+
+  globalArray->replaceAllUsesWith(newGlobalArray);
+  globalArray->eraseFromParent();
 }
 
-bool DataFlowTransformation::runOnFunction(Function &F) {
-  if (!toObfuscate(flag, &F, "dft")) {
-    return false;
+void DataFlowTransformation::runOnFunction(Function &F) {
+  // Define types
+  IntegerType *Int32 = IntegerType::get(F.getContext(), 32);
+  PointerType *IntPointerType = PointerType::get(Int32, 0);
+
+  for (Function::iterator bb = F.begin(), bbE = F.end(); bb != bbE; ++bb) {
+    for (BasicBlock::iterator inst = bb->begin(), instE = bb->end(); inst != instE; ++inst) {
+      // There are some instructions which we don't want to mess with
+      if (dyn_cast<AllocaInst>(inst)) {
+        continue; 
+      } else if (dyn_cast<StoreInst>(inst)) {
+        continue;
+      }
+
+      for (Use *operand = inst->op_begin(), *operandE = inst->op_end(); operand != operandE; ++operand) {
+        if (ConstantInt *CI = dyn_cast<ConstantInt>(operand->get())) {
+          Module *M = F.getParent();
+
+          // Get the array and current index
+          GlobalVariable *globalArray = M->getNamedGlobal(getArrayName(*M).str());
+          ConstantInt *arrayIndex = ConstantInt::get(F.getContext(), currentIndex);
+
+          // Manage the actual index that is used
+          std::map<APSInt, ConstantInt *>::iterator it;
+          bool result;
+          std::tie(it, result) = intToIndex.emplace(APSInt(CI->getValue()), arrayIndex);
+          if (result) {
+            currentIndex++;
+          }
+
+          GetElementPtrInst *gepInst = GetElementPtrInst::Create(globalArray, ArrayRef<Value *>(it->second), "array" + it->second->getValue().toString(10, false), inst);
+          *operand = gepInst;
+
+          break;
+        } else {
+          continue;
+        }
+      }
+    }
   }
+}
+
+Twine DataFlowTransformation::getArrayName(Module &M) {
+  return globalArrayBaseName + "_" + M.getModuleIdentifier();
 }
